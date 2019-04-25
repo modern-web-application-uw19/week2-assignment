cec65f9d74bea9d6f6797f33401be4733ca8227f
Colin Griffiths <colinrossgriffiths@gmail.com>
2019-04-25 15:01:27 -0700

    Adding onClick action to article card

--- social-network/src/components/Article.js
+++ social-network/src/components/Article.js
@@ -9,17 +9,21 @@ import './ArticleImage.css';
 class Article extends React.Component {
   constructor(props) {
     super(props);
+    this.openArticle = this.openArticle.bind(this);
+  }
+
+  openArticle() {
+    window.open(this.props.article.link, '_blank');
   }
 
   render() {
     return (
       <div className={`article-grid__container-${this.props.articleType}`}>
-        <div className={ `article d-flex flex-${this.props.articleType}` }>
+        <div className={ `article d-flex flex-${this.props.articleType}` } onClick={ () => window.open(this.props.article.link, '_blank')}>
           <div className={ `article-image_wrapper align-items-stretch p-0` }>
             <ArticleImage src={this.props.article.image} title={this.props.article.title} />
           </div>
           <div className={`d-flex flex-column justify-content-between p-3`}>
-            
             <Summary article={ this.props.article }/>
             <DetailBlock article={ this.props.article } />
           </div>
